a5aabad652c50d4119f14f24386149c5a07f2ca5
ferlanymh <45175115+ferlanymh@users.noreply.github.com>
2019-01-28 14:08:40 +0800

    更新做法，用栈实现，代码更简洁

--- 单链表的逆置.cpp
+++ 单链表的逆置.cpp
@@ -47,3 +47,42 @@ public:
         
     }
 };
+
+
+
+//做法二 ，用栈存结点，再依次出结点
+//用栈更好
+/*
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+            val(x), next(NULL) {
+    }
+};*/
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+        if (pHead ==nullptr || pHead->next ==nullptr)
+        {
+            return pHead;
+        }
+        stack<ListNode*> s;
+        ListNode* cur = pHead;
+        while (cur->next)//挨个入栈，留最后一个结点
+        {
+            s.push(cur);
+            cur = cur->next;
+        }
+        pHead = cur;//最后一个作头部
+         
+        while (!s.empty())//依次取出结点链在最后
+        {
+            cur->next = s.top();
+            cur = cur->next;
+            s.pop();
+        }
+        cur->next = nullptr;
+        return pHead;
+    }
+};
