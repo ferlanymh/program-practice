e8fd7030e48b06a24563e45fb4068fe5051699bb
ferlanymh <45175115+ferlanymh@users.noreply.github.com>
2019-02-15 19:38:02 +0800

    在一个字符串找到第一个只出现一次的字符,并返回它的位置, 如果没有则返回 -1（需要区分大小写）.

---
+++ 字符串中第一个只出现一次的字符.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class BitSet
+{
+public:
+	BitSet(size_t len)//len是要表示数据的最大值
+	{
+		BitTable.resize((len >> 3) + 1);//一个字节可以表示8个数，用len / 8就可以得知要有多少字节表示。
+										//+1是因为有可能除出来有小数，为了防止不够放故+1.
+	}
+	void InputBitSet(int num)
+	{
+		size_t pos = num / 8;//确定是在哪一个字节
+		size_t index = pos % 8;//确定是在字节的哪一位
+
+		BitTable[pos] |= ((char)1 << index);
+
+	}
+
+	void removeBitSet(int num)
+	{
+		size_t pos = num / 8;
+		size_t index = pos % 8;
+		BitTable[pos] &= ~((char)1 << index);
+	}
+
+	bool FindBitSet(int num)
+	{
+		size_t pos = num / 8;
+		size_t index = pos % 8;
+		if (BitTable[pos] & ((char)1 << index) == 0)
+			return false;
+		
+		return true;
+
+	}
+private:
+	vector<char> BitTable;
+};
