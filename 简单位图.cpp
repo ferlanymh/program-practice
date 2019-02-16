ce7aa7892c4a57b4308f43929b5591c29334d36e
ferlanymh <45175115+ferlanymh@users.noreply.github.com>
2019-02-16 09:21:16 +0800

    Update 简单位图.cpp

--- 简单位图.cpp
+++ 简单位图.cpp
@@ -12,26 +12,26 @@ public:
 		BitTable.resize((len >> 3) + 1);//一个字节可以表示8个数，用len / 8就可以得知要有多少字节表示。
 										//+1是因为有可能除出来有小数，为了防止不够放故+1.
 	}
-	void InputBitSet(int num)
+	void InputBitSet(int num)//在位图中标记某一个数
 	{
 		size_t pos = num / 8;//确定是在哪一个字节
-		size_t index = pos % 8;//确定是在字节的哪一位
+		size_t index = num % 8;//确定是在字节的哪一位
 
 		BitTable[pos] |= ((char)1 << index);
 
 	}
 
-	void removeBitSet(int num)
+	void removeBitSet(int num)//在位图中删除一个数的标记
 	{
 		size_t pos = num / 8;
-		size_t index = pos % 8;
+		size_t index = num % 8;
 		BitTable[pos] &= ~((char)1 << index);
 	}
 
-	bool FindBitSet(int num)
+	bool FindBitSet(int num)//在位图中查找一个数的标记
 	{
 		size_t pos = num / 8;
-		size_t index = pos % 8;
+		size_t index = num % 8;
 		if (BitTable[pos] & ((char)1 << index) == 0)
 			return false;
 		
